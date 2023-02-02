76c251224d9c2cab141979833d77895e2bc61a7e
Shubarina <shubaru@mail.ru>
2023-02-02 14:25:27 +0500

    вторая редакция спринта 6

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 
 def page_not_found(request, exception):
     return render(
-        request, 'core/404.html', {'path': request.path}, HTTPStatus.NOT_FOUND
+        request,
+        'core/404.html', {'path': request.path},
+        status=HTTPStatus.NOT_FOUND
     )
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html', HTTPStatus.FORBIDDEN)
+    return render(request, 'core/403csrf.html', status=HTTPStatus.FORBIDDEN)
